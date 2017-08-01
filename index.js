6a9b6383a02ffb80d51d73c5c68de378c4e08b9a
Wagner Andrade <andradewagner@hotmail.com>
2017-08-01 10:56:41 -0300

    node app paginas e links

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/time', function(request, response) {
+  response.render('pages/time');
+});
+
+app.get('/quem_somos', function(request, response) {
+  response.render('pages/quem_somos');
+});
+
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Aplicação Node rodando na porta', app.get('port'));
 });
